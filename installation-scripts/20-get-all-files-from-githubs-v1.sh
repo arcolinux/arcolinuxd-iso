b3a93c6a0081c88b16747e82c1a24afd56c9a27c
Erik Dubois <erik.dubois@gmail.com>
2018-02-26 18:51:23 +0100

    trials with use of neofetch stopped - neofetch not standard installed

--- installation-scripts/20-get-all-files-from-githubs-v1.sh
+++ installation-scripts/20-get-all-files-from-githubs-v1.sh
@@ -18,6 +18,3 @@ rm -rf ../archiso/airootfs/etc/skel/.bashrc
 echo "getting .bashrc from iso-skel"
 cp -r ../../arcolinux-root/root/.bashrc-latest ../archiso/airootfs/etc/skel/.bashrc
 echo ".bashrc copied to /etc/skel"
-
-echo "settings for neofetch"
-cp -r ../../arcolinux-iso-skel/config/neofetch/config ../archiso/airootfs/etc/skel/.config/neofetch/config
