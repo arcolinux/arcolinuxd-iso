0d1fb7daf7b121a7118f9746837385f25bc7092e
Erik Dubois <erik.dubois@gmail.com>
2018-03-02 23:12:23 +0100

    2 move scripts : betatesters and coremembers

---
+++ installation-scripts/50-move-the-iso-to-coremembers.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# ArcoLinux	: 	https://arcolinux.com/
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "moving iso to documents"
+cp ~/arcolinuxd-build/archiso/out/arcolinuxd* ~/Documents/
+
+echo "moving iso to insync just for Teammembers only"
+cp ~/arcolinuxd-build/archiso/out/arcolinuxd* ~/Insync/SHARED/TeamMembers-Only/
+
+#echo "moving iso to insync ArcoLinux-A-Team"
+#cp ~/arcolinuxd-build/archiso/out/arcolinuxd* ~/Insync/SHARED/ArcoLinux-A-Team/
