0f20deab746ddeb12688a8b459d1a1d3339390c6
Erik Dubois <erik.dubois@gmail.com>
2018-12-01 10:01:31 +0100

    cleanup script out of system package

---
+++ archiso/airootfs/usr/bin/cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+chmod 750 /etc/sudoers.d
+chmod 750 /etc/polkit-1/rules.d
+rm -f /etc/sudoers.d/g_wheel
+rm -rf /usr/lib/calamares
+rm -rf /etc/skel/.config/xfce4
+rm -rf /etc/skel/.config/Thunar
+rm -f /etc/polkit-1/rules.d/49-nopasswd_global.rules
+rm -r /etc/systemd/system/etc-pacman.d-gnupg.mount
+rm /root/{.automated_script.sh,.zlogin}
+rm /etc/mkinitcpio-archiso.conf
+rm -r /etc/initcpio
+rm /usr/bin/cleanup.sh
