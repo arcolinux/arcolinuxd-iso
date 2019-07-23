474f51ad9b8e6268400bb5d4d7c7dfe25e1cb7e3
Erik Dubois <erik.dubois@gmail.com>
2019-07-23 13:12:37 +0200

    bump number

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinuxd
-iso_label="arcolinuxd-v19.07.12"
+iso_label="arcolinuxd-v19.07.13"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinuxD Live/Rescue CD"
-iso_version="v19.07.12"
+iso_version="v19.07.13"
 install_dir=arch
 work_dir=work
 out_dir=out
