eb4140738d32a5ccfd2cd1aa3f5a424b4fc791cd
Erik Dubois <erik.dubois@gmail.com>
2020-01-31 16:06:10 +0100

    bump version for awesome font

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinuxd
-iso_label="arcolinuxd-v20.2.9"
+iso_label="arcolinuxd-v20.2.10"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinuxD Live/Rescue CD"
-iso_version="v20.2.9"
+iso_version="v20.2.10"
 install_dir=arch
 work_dir=work
 out_dir=out
