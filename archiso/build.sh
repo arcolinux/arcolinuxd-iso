b1f2da1835f46d03a6e77db5e9397348dc921a14
Erik Dubois <erik.dubois@gmail.com>
2020-05-01 10:53:25 +0200

    new version for new calamares config

--- archiso/build.sh
+++ archiso/build.sh
@@ -3,10 +3,10 @@
 set -e -u
 
 iso_name=arcolinuxd
-iso_label="arcolinuxd-v20.5.5"
+iso_label="arcolinuxd-v20.5.6"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinuxD Live/Rescue CD"
-iso_version="v20.5.5"
+iso_version="v20.5.6"
 install_dir=arch
 work_dir=work
 out_dir=out
