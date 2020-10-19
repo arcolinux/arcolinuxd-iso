56f25e350c50055c093571c2af1de19fcadaafbb
Erik Dubois <erik.dubois@gmail.com>
2020-10-19 06:28:54 +0200

    bump nr for new calamares config

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name=arcolinuxd
-iso_label="arcolinuxd-v20.11.8"
+iso_label="arcolinuxd-v20.11.9"
 iso_publisher="ArcoLinux <http://www.arcolinux.info>"
 iso_application="ArcoLinux Live/Rescue CD"
-iso_version="v20.11.8"
+iso_version="v20.11.9"
 install_dir=arch
 work_dir=work
 out_dir=out
