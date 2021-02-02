0686b36bed6a157180fd134637ce8d944d82690c
Erik Dubois <erik.dubois@gmail.com>
2021-02-02 13:32:26 +0100

    update

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -23,4 +23,5 @@ file_permissions=(
   ["/usr/local/bin/arcolinux-before"]="0:0:755"
   ["/usr/local/bin/arcolinux-final"]="0:0:755"
   ["/usr/local/bin/arcolinux-displaymanager-check"]="0:0:755"
+  ["/usr/local/bin/arcolinux-skel"]="0:0:755"
 )
