67f0dc0a44fe7eca8dfa0d5bf85ee03238aca4d5
Erik Dubois <erik.dubois@gmail.com>
2021-06-01 12:49:17 +0200

    archiso version

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -34,7 +34,7 @@ echo
 	isoLabel='arcolinuxd-'$arcolinuxVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 54-1"
+	archisoRequiredVersion="archiso 55-1"
 	buildFolder=$HOME"/arcolinuxd-build"
 	outFolder=$HOME"/ArcoLinuxD-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
