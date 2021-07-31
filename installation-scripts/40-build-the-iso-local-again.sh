6b5e6727ed4fe5d0f5aa7257e1b79d7b81ae22df
Erik Dubois <erik.dubois@gmail.com>
2021-07-31 08:37:41 +0200

    archiso version 57-1

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,12 +29,12 @@ echo
 	desktop="xfce"
 	dmDesktop="xfce"
 
-	arcolinuxVersion='v21.09.3'
+	arcolinuxVersion='v21.09.4'
 
 	isoLabel='arcolinuxd-'$arcolinuxVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 56.1-1"
+	archisoRequiredVersion="archiso 57-1"
 	buildFolder=$HOME"/arcolinuxd-build"
 	outFolder=$HOME"/ArcoLinuxD-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
